0fa8a6c9192ea7f5210230a26970def8ba5b8274
pavanery <pavanery@gmail.com>
2018-01-14 02:20:16 -0300

    Create base application Kotlin + Express

---
+++ src/main/kotlin/KekApp.kt
@@ -0,0 +1,17 @@
+external fun require(module: String): dynamic
+
+fun main(args: Array<String>) {
+    println("Hello KEK!")
+
+    val express = require("express")
+    val app = express()
+
+    app.get("/", { req, res ->
+        res.type("text/plain")
+        res.send("Hello KEK!")
+    })
+
+    app.listen(3000, {
+        println("Listening on port 3000")
+    })
+}
