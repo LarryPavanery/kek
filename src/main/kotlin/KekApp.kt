ddd1290de4a55d9c0fc7f2dabebfeeb01d483c92
pavanery <pavanery@gmail.com>
2018-01-14 15:21:07 -0300

    add process module to resolve port on Heroku

--- src/main/kotlin/KekApp.kt
+++ src/main/kotlin/KekApp.kt
@@ -1,17 +1,17 @@
 external fun require(module: String): dynamic
 
 fun main(args: Array<String>) {
-    println("Hello KEK!")
-
     val express = require("express")
+    val process = require("process")
     val app = express()
+    val port = process.env.PORT ?: 3000
 
     app.get("/", { _, res ->
         res.type("text/plain")
         res.send("Hello KEK!")
     })
 
-    app.listen(3000, {
-        println("Listening on port 3000")
+    app.listen(port, {
+        println("Listening on port $port")
     })
 }
